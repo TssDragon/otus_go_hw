4c458ed4b2e9b528972c54e6503703caffcd0458
Pavel Vinogradov <p.vinogradov@cbonds.info>
2021-11-25 22:41:52 +0300

    hw 02 first iteration

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,57 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(packedString string) (string, error) {
+
+	var result strings.Builder
+	var prevRune rune
+
+	for _, currRune := range packedString {
+
+		isDigitCurrentChar := unicode.IsDigit(currRune)
+		isDigitPrevChar := unicode.IsDigit(prevRune)
+		currChar := string(currRune)
+
+		if prevRune == 0 {
+			if isDigitCurrentChar {
+				return "", ErrInvalidString
+			}
+			prevRune = currRune
+			continue
+		}
+
+		if isDigitCurrentChar && !isDigitPrevChar {
+
+			repeat, _ := strconv.Atoi(currChar)
+			result.WriteString(strings.Repeat(string(prevRune), repeat))
+			prevRune = 0
+			continue
+		}
+
+		if isDigitCurrentChar && isDigitPrevChar {
+			return "", ErrInvalidString
+		}
+
+		if !isDigitCurrentChar {
+			result.WriteRune(prevRune)
+			prevRune = currRune
+		}
+	}
+
+	if prevRune != 0 && unicode.IsDigit(prevRune) {
+		return "", ErrInvalidString
+	}
+
+	if prevRune != 0 {
+		result.WriteRune(prevRune)
+	}
+
+	return result.String(), nil
 }
