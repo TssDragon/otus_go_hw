a722427a31380908223bed8f739f10b690c31776
Pavel Vinogradov <p.vinogradov@cbonds.info>
2021-12-03 21:45:57 +0300

    hw 03 complete

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+func Top10(inStr string) []string {
+	replaceRegExp := regexp.MustCompile(`([.,;]+)|([^a-zA-Zа-яА-Я]+-[^a-zA-Zа-яА-Я]+)`)
+	inStr = replaceRegExp.ReplaceAllString(inStr, " ")
+
+	inStr = strings.ToLower(inStr)
+
+	splitRegExp := regexp.MustCompile(`\s+`)
+	splitWords := splitRegExp.Split(inStr, -1)
+
+	wordsMap := make(map[string]int)
+	for _, word := range splitWords {
+		if word == "" || word == "-" {
+			continue
+		}
+
+		_, keyExists := wordsMap[word]
+		if !keyExists {
+			wordsMap[word] = 1
+		} else {
+			wordsMap[word] += 1
+		}
+	}
+
+	type userMap struct {
+		Key   string
+		Value int
+	}
+
+	var wordsSlice []userMap
+	for k, v := range wordsMap {
+		wordsSlice = append(wordsSlice, userMap{k, v})
+	}
+
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		if wordsSlice[i].Value == wordsSlice[j].Value {
+			return wordsSlice[i].Key < wordsSlice[j].Key
+		}
+		return wordsSlice[i].Value > wordsSlice[j].Value
+	})
+
+	var result []string
+	for _, val := range wordsSlice {
+		result = append(result, val.Key)
+
+		if len(result) == 10 {
+			break
+		}
+	}
+
+	return result
 }
