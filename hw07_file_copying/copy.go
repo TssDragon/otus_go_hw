99bf79de274d00fa3e5e8c8ed259b1438f3b2eda
Pavel Vinogradov <p.vinogradov@cbonds.info>
2022-03-06 19:55:45 +0300

    hw 07 complete

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,117 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrEmptyPath             = errors.New("from/to path must be set")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	err := checkPathsError(fromPath, toPath)
+	if err != nil {
+		return err
+	}
+
+	err = checkFileSizeAndOffset(fromPath, offset)
+	if err != nil {
+		return err
+	}
+
+	currFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	defer closeFileAndProgressBarIfExists(currFile, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = currFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	bytesForCopy := setRealBytesForCopy(currFile, limit, offset)
+
+	err = realCopy(currFile, toPath, bytesForCopy)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func checkPathsError(paths ...string) error {
+	for _, path := range paths {
+		if path == "" {
+			return ErrEmptyPath
+		}
+	}
+
+	return nil
+}
+
+func checkFileSizeAndOffset(path string, offset int64) error {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	return nil
+}
+
+func setRealBytesForCopy(file *os.File, limit int64, offset int64) int64 {
+	fileStat, _ := file.Stat()
+	fileSize := fileStat.Size()
+
+	bytesForCopy := fileSize - offset
+
+	if limit < bytesForCopy && limit > 0 {
+		bytesForCopy = limit
+	}
+
+	return bytesForCopy
+}
+
+func realCopy(from *os.File, path string, limit int64) error {
+	newFile, err := os.Create(path)
+	progressBar := pb.Full.Start64(limit)
+
+	defer closeFileAndProgressBarIfExists(newFile, progressBar)
+	if err != nil {
+		return err
+	}
+
+	proxyReader := progressBar.NewProxyReader(from)
+
+	_, err = io.CopyN(newFile, proxyReader, limit)
+	if err != nil {
+		return err
+	}
+
+	progressBar.Finish()
+
+	return nil
+}
+
+func closeFileAndProgressBarIfExists(file *os.File, bar *pb.ProgressBar) {
+	if file != nil {
+		file.Close()
+	}
+
+	if bar != nil {
+		bar.Finish()
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,77 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testFilePathFrom = "testdata/input.txt"
+	testFilePathTo   = "testdata/result.txt"
+)
+
+func TestCopyNegative(t *testing.T) {
+	t.Run("file not found", func(t *testing.T) {
+		from := "/there/is/no/file"
+		to := "/"
+
+		result := Copy(from, to, 0, 0)
+		require.ErrorIs(t, result, os.ErrNotExist)
+	})
+
+	t.Run("empty paths", func(t *testing.T) {
+		from := "/there/is/no/file"
+		emptyFrom := ""
+		emptyTo := ""
+
+		emptyFromResult := Copy(emptyFrom, testFilePathTo, 0, 0)
+		emptyToResult := Copy(from, emptyTo, 0, 0)
+
+		require.ErrorIs(t, emptyFromResult, ErrEmptyPath)
+		require.ErrorIs(t, emptyToResult, ErrEmptyPath)
+	})
+
+	t.Run("offset more than file size", func(t *testing.T) {
+		to := "/"
+		var offset int64 = 1024 * 1024 * 1024
+
+		result := Copy(testFilePathFrom, to, offset, 0)
+		require.ErrorIs(t, result, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("make new file in restricted directory", func(t *testing.T) {
+		to := "/dev/go_test.txt"
+
+		result := Copy(testFilePathFrom, to, 0, 0)
+		require.ErrorIs(t, result, os.ErrPermission)
+	})
+}
+
+func TestCopyPositive(t *testing.T) {
+	t.Run("full copy is correct", func(t *testing.T) {
+		Copy(testFilePathFrom, testFilePathTo, 0, 0)
+
+		fromStat, _ := os.Stat(testFilePathFrom)
+		toStat, _ := os.Stat(testFilePathTo)
+
+		fromSize := fromStat.Size()
+		toSize := toStat.Size()
+		os.Remove(testFilePathTo)
+
+		require.Equal(t, fromSize, toSize)
+	})
+
+	t.Run("partial copy is correct", func(t *testing.T) {
+		var limit int64 = 10
+
+		Copy(testFilePathFrom, testFilePathTo, 0, limit)
+
+		toStat, _ := os.Stat(testFilePathTo)
+		toSize := toStat.Size()
+		os.Remove(testFilePathTo)
+
+		require.Equal(t, toSize, limit)
+	})
 }
