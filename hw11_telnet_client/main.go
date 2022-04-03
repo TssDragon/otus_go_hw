e20480ac9a01e48ba8d7ec0cea2aa5362912e1d3
Pavel Vinogradov <p.vinogradov@cbonds.info>
2022-04-03 13:39:38 +0300

    hw 11 complete

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,67 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
+const DefaultTimeout = 10
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", DefaultTimeout*time.Second, "connection timeout")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		log.Fatal("Address & port must be type")
+	}
+
+	host := args[0]
+	port := args[1]
+	socket := net.JoinHostPort(host, port)
+
+	telnetClient := NewTelnetClient(socket, timeout, os.Stdin, os.Stdout)
+	if err := telnetClient.Connect(); err != nil {
+		log.Fatalf("Error while open connection: %s", err)
+	}
+	defer telnetClient.Close()
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	go writer(telnetClient, cancelFunc)
+	go reader(telnetClient, cancelFunc)
+
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+	select {
+	case <-signalChannel:
+		cancelFunc()
+		signal.Stop(signalChannel)
+		return
+
+	case <-ctx.Done():
+		close(signalChannel)
+		return
+	}
+}
+
+func writer(client TelnetClient, cancelFunction context.CancelFunc) {
+	if err := client.Send(); err != nil {
+		cancelFunction()
+	}
+}
+
+func reader(client TelnetClient, cancelFunction context.CancelFunc) {
+	if err := client.Receive(); err != nil {
+		cancelFunction()
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"net"
 	"time"
 )
+
+var ErrHasNoOpenConnection = errors.New("there is no open connection")
 
 type TelnetClient interface {
 	Connect() error
@@ -12,10 +16,53 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type Client struct {
+	address    string
+	timeout    time.Duration
+	in         io.ReadCloser
+	out        io.Writer
+	connection net.Conn
+}
+
+func (c *Client) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+	c.connection = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Close() error {
+	if c.connection == nil {
+		return nil
+	}
+	return c.connection.Close()
+}
+
+func (c *Client) Send() error {
+	if c.connection == nil {
+		return ErrHasNoOpenConnection
+	}
+
+	_, err := io.Copy(c.connection, c.in)
+	return err
+}
+
+func (c *Client) Receive() error {
+	if c.connection == nil {
+		return ErrHasNoOpenConnection
+	}
+
+	_, err := io.Copy(c.out, c.connection)
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
